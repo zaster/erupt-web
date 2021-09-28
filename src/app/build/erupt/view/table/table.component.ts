418139bfb20bc2679bc1af2e5244dca2c16a6610
Hansch <zasterliu@gmail.com>
2021-09-28 15:08:55 +0800

    bug fix update

--- src/app/build/erupt/view/table/table.component.ts
+++ src/app/build/erupt/view/table/table.component.ts
@@ -407,7 +407,7 @@ export class TableComponent implements OnInit {
                 }
             });
         } else if(ro.type==OperationType.IMPORT)  {
-            let paramData ={parent:this._drill?.eruptParent,parentId:this._drill?.val};
+            let paramData =this._drill?{parent:this._drill.eruptParent,parentId:this._drill.val}:{};
             let model = this.modal.create({
                 nzKeyboard: true,
                 nzTitle: "Excel导入",
